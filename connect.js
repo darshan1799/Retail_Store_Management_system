0365e7bd34664ce4f8122e968517809f475b6894
darshan sutariya <pateldarshan1322005@gmail.com>
2024-07-02 15:57:34 +0530

    Add Delete Buttons And Solve Bug Related To the Quantity

--- connect.js
+++ connect.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose');
-let connect = mongoose.connect('mongodb+srv://darshan:darshan123@cluster0.3zzlor5.mongodb.net/mydata');
+let connect = mongoose.connect("mongodb://localhost:27017/task2");
+// let connect = mongoose.connect('mongodb+srv://darshan:darshan123@cluster0.3zzlor5.mongodb.net/mydata');
 module.exports = connect;
